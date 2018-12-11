5760f1ad8e7fa4de251c050d4dcb5b764fe2e7be
Cecile Veneziani <contact@cecilitse.org>
2018-12-12 00:55:03 +0100

    Add some routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
+
+  resources :plants, only: [:index, :show]
+  resource  :leaderboard, only: [:show]
+
+  namespace :garden do
+    root to: 'plants#index'
+
+    resources :plants do
+      resources :care_moments, only: [:create]
+    end
+  end
 end
