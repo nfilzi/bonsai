326390e96cc7feb4a1ec0d4a4f02ede471691e91
Cecile Veneziani <contact@cecilitse.org>
2018-12-12 02:17:17 +0100

    Add plant page

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,4 +2,12 @@ class PlantsController < ApplicationController
   def index
     @plants = Plant.order("created_at DESC")
   end
+
+  def show
+    @plant = Plant.find(params[:id])
+
+    @gardener_other_plants = Plant.select do |plant|
+      plant != @plant && plant.user == @plant.user
+    end
+  end
 end
