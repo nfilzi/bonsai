792d1ce6704a26a4e1e6a0da69b631aef887b66d
Cecile Veneziani <contact@cecilitse.org>
2018-12-12 21:09:55 +0100

    Add plant creation

--- app/controllers/garden/plants_controller.rb
+++ app/controllers/garden/plants_controller.rb
@@ -4,4 +4,37 @@ class Garden::PlantsController < ApplicationController
   def index
     @plants = current_user.plants
   end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    @plant.user = current_user
+
+    if @plant.save
+      flash[:notice] = 'Your plant was successfully added to your garden.'
+      redirect_to garden_plant_path(@plant)
+    else
+      flash.now[:alert] = 'Unable to add your plant'
+      render :new
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(
+      :name,
+      :age_in_months,
+      :size,
+      :room,
+      :photo_url
+    )
+  end
+
+  def set_plant
+    @plant = current_user.plants.find(params[:id])
+  end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,7 @@
 class Plant < ApplicationRecord
+  ROOMS = %w(entrance living-room bedroom bathroom office)
+  SIZES = %w(small medium tall)
+
   belongs_to :user
   has_many :care_moments, dependent: :destroy
 
