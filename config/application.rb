8acbc87939ada0b9563e6c10d7cbba6be517810a
Cecile Veneziani <contact@cecilitse.org>
2018-12-14 00:50:06 +0100

    Disable assets and helpers generators

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ Bundler.require(*Rails.groups)
 
 module Bonsai
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
