ec838674e1e0db2ebf10e094479116e9f8a4e22f
Nicolas Filzi <nfilzi.webservices@gmail.com>
2019-04-08 15:30:44 +0200

    Move MOMENTS to its own file

--- app/models/care_moment.rb
+++ app/models/care_moment.rb
@@ -1,19 +1,4 @@
 class CareMoment < ApplicationRecord
-  MOMENTS = {
-    weed: {
-      points: 2, # per month : 4
-      min_frequency_in_days: 15
-    },
-    water: {
-      points: 10, # per month : 80
-      min_frequency_in_days: 3
-    },
-    repot: {
-      points: 100, # per month : 25
-      min_frequency_in_days: 300
-    }
-  }
-
   belongs_to :plant
 
   validates :code,   presence: true, inclusion: { in: MOMENTS.keys.map(&:to_s) }
---
+++ app/models/care_moment/moments.rb
@@ -0,0 +1,16 @@
+class CareMoment
+  MOMENTS = {
+    weed: {
+      points: 2, # per month : 4
+      min_frequency_in_days: 15
+    },
+    water: {
+      points: 10, # per month : 80
+      min_frequency_in_days: 3
+    },
+    repot: {
+      points: 100, # per month : 25
+      min_frequency_in_days: 300
+    }
+  }
+end
