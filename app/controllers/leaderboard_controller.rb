96f8b9cf1e1fdcddcace9701dcb621950d22fbee
Nicolas Filzi <nfilzi.webservices@gmail.com>
2019-04-08 16:27:50 +0200

    Fix N + 1 on care moments count & plants count for leaderboard

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,5 +1,13 @@
 class LeaderboardController < ApplicationController
   def show
-    @top_users = User.order('level DESC, care_points DESC').limit(10)
+    @top_users = User.select(
+        "users.*,
+        COUNT(DISTINCT plants.id) AS plants_count,
+        COUNT(*)                  AS care_moments_count"
+      ).
+      joins(:plants, :care_moments).
+      order('level DESC, users.care_points DESC').
+      group("plants.user_id").
+      limit(10)
   end
 end
