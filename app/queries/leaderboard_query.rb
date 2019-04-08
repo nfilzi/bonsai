469e053a4853ed0c70cbfbb5e002bcecf7bcfd89
Nicolas Filzi <nfilzi.webservices@gmail.com>
2019-04-08 18:51:38 +0200

    Add LEFT JOIN

--- app/queries/leaderboard_query.rb
+++ app/queries/leaderboard_query.rb
@@ -7,9 +7,9 @@ class LeaderboardQuery < BaseQuery
       COUNT(DISTINCT plants.id) AS plants_count,
       COUNT(*)                  AS care_moments_count"
     ).
-    joins(:plants, :care_moments).
+    left_outer_joins(:plants, :care_moments).
     order('level DESC, users.care_points DESC').
-    group("plants.user_id").
+    group("users.id").
     limit(10)
   end
 end
