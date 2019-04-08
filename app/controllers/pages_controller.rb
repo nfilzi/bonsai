bce476be7230466f5511e6503c3b88e30c83da79
Nicolas Filzi <nfilzi.webservices@gmail.com>
2019-04-08 15:32:54 +0200

    Fix N + 1 home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @latest_plants = Plant.order("created_at DESC").limit(3)
+    @latest_plants = Plant.eager_load(:user).order("plants.created_at DESC").limit(3)
   end
 end
