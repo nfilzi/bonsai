70ff84c2ad156333ec65d9462563220560a619ca
Nicolas Filzi <nfilzi.webservices@gmail.com>
2019-04-10 16:54:52 +0200

    Add Plant.scopes tests (needing_care & include_status)

--- spec/factories/plants.rb
+++ spec/factories/plants.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     age_in_months  { 3 }
     size           { 'small' }
     photo_url      { 'https://img-3.journaldesfemmes.fr/reCRzYkzv7psZov1ygWXI40WtQE=/910x607/smart/1c0c9fde8379424f9a32318875da1203/ccmcms-jdf/10950238.jpg' }
+    care_moments   { [] }
+  end
+
+  trait :with_owner do
+    association :owner, factory: :users
   end
 end
--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -15,6 +15,46 @@ RSpec.describe Plant, type: :model do
     end
   end
 
+
+  describe "scopes" do
+    describe ".needing_care" do
+      before :each do
+        @plants_needing_care = create_list(:plants, 2, :with_owner)
+
+        care_moments = [
+          CareMoment.new(code: :water, points: 10,  date: Date.today),
+          CareMoment.new(code: :repot, points: 100, date: Date.today),
+          CareMoment.new(code: :weed,  points: 20,  date: Date.today)
+        ]
+
+        @plant_not_needing_care = create(:plants, :with_owner, care_moments: care_moments)
+      end
+
+      it "returns only plants which need care from their owners" do
+        expect(Plant.needing_care).to match_array(@plants_needing_care)
+      end
+    end
+
+    describe ".include_care_status" do
+      before :each do
+        care_moments = [
+          CareMoment.new(code: :water, points: 10,  date: Date.today),
+          CareMoment.new(code: :repot, points: 100, date: Date.today)
+        ]
+
+        @plant_not_needing_care = create(:plants, :with_owner, care_moments: care_moments)
+      end
+
+      it "returns plants enhanced with their care status" do
+        plant = Plant.include_care_status.first
+
+        expect(plant.weed_needed).to eq(1)
+        expect(plant.water_needed).to eq(0)
+        expect(plant.repot_needed).to eq(0)
+      end
+    end
+  end
+
   describe "validations" do
     it "validates name is present" do
       expect(plant).to validate_presence_of(:name)
